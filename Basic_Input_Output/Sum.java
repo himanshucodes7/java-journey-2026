2056221b4aa9de566b6d76673fac2cf6bc82f586
Himanshu Raj <himanshu44226688@gmail.com>
2026-01-29 01:40:20 +0530

    day1 program on sum

---
+++ Basic_Input_Output/Sum.java
@@ -0,0 +1,11 @@
+package Basic_Input_Output;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 24;
+        int b = 62;
+        int sum = a + b;
+        
+        System.out.println(sum);
+    }
+}
