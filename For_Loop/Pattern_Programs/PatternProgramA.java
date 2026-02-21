22cceb54f965d6881dd5867a83321aa89b72ad6b
Himanshu Raj <himanshu44226688@gmail.com>
2026-02-21 21:48:12 +0530

    new 8 programs added on Patterns

---
+++ For_Loop/Pattern_Programs/PatternProgramA.java
@@ -0,0 +1,15 @@
+package For_Loop.Pattern_Programs;
+
+public class PatternProgramA {
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 5; i++)
+        {
+            for(int j = 1; j <=i; j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }    
+}
