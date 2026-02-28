b1a2b2f902d68ecc54fbc23f54c4c312351043f7
Himanshu Raj <himanshu44226688@gmail.com>
2026-02-28 23:07:20 +0530

    sum program

---
+++ Basic_Input_Output/variabletest.java
@@ -0,0 +1,14 @@
+package Basic_Input_Output;
+
+public class variabletest {
+    public static void main(String[] args) {
+        
+        int a = 8;
+        int b = 99;
+        int c = 45;
+
+        int sum = a + b + c;
+
+        System.out.println(sum);
+    }
+}
