6d96aafe8e46e40fa81a2b7ed24e9a7434eb8f0c
Himanshu Raj <himanshu44226688@gmail.com>
2026-03-12 23:02:25 +0530

    new program added

---
+++ String/CountingParticularCharacter.java
@@ -0,0 +1,21 @@
+package String;
+import java.util.*;
+public class CountingParticularCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+
+        int count = 0;
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            if (ch == 'A' || ch == 'a') {
+                count++;
+            }
+        }
+        System.out.println("No of 'A' or 'a' = " + count);
+        sc.close();
+    }
+}
