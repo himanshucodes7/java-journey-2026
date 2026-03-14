2460a5fd3fb3cb13812f29105d7b24ae503d258c
Himanshu Raj <himanshu44226688@gmail.com>
2026-03-14 22:27:11 +0530

    new program added

---
+++ Basic_Input_Output/addtwonumbers.java
@@ -0,0 +1,11 @@
+package Basic_Input_Output;
+
+public class addtwonumbers {
+    public static void main(String[] args) {
+        int a = 67;
+        int b = 84;
+
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
