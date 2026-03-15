4aaafc89eeab7433267f3d1eedd50a61b44fef5b
Himanshu Raj <himanshu44226688@gmail.com>
2026-03-15 22:22:36 +0530

    new program added

---
+++ Basic_Input_Output/Subtract.java
@@ -0,0 +1,9 @@
+package Basic_Input_Output;
+
+public class Subtract {
+    public static void main(String[] args) {
+        int a = 56;
+        int b = 12;
+        System.out.println(a-b);
+    }
+}
